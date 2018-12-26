dbf49f9abea3714c82ea539e5ae464a0d1c85d26
Kinshuk <kinshukkislay619@gmail.com>
2018-12-27 01:03:47 +0530

    First Commit

---
+++ src/products/product-list.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { IProduct } from 'src/products/product';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+
+@Component({
+    selector : 'pm-products',
+    templateUrl : './product-list.component.html',
+    styleUrls : ['./product-list.component.css']
+})
+
+export class ProductListComponent implements OnInit{
+    pageTitle : string = 'Product List!' ;
+    imageWidth : number = 50;
+    imageMargin :number = 2;
+    showImage : boolean = false;
+
+    //listFilter : string = 'cart';
+    _listFilter : string ;
+    get listFilter() : string{
+        return this._listFilter;
+    }
+    set listFilter (value : string)
+    {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) :this.products;
+    }
+
+    filteredProducts : IProduct[];
+    products : IProduct[] = [
+        {
+            "productId": 1,
+            "productName": "Leaf Rake",
+            "productCode": "GDN-0011",
+            "releaseDate": "March 19, 2016",
+            "description": "Leaf rake with 48-inch wooden handle.",
+            "price": 19.95,
+            "starRating": 3.2,
+            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+          },
+          {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2016",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+          }
+        ];
+        toggleImage() : void{
+            this.showImage = !this.showImage;
+        }
+        ngOnInit() : void{
+            console.log('In OnInit');
+        } 
+        performFilter(filterBy : string ) : IProduct{
+            filterBy = filterBy.trimLeft();    
+        }
+}
